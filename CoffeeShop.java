48a78f3a6fd62dd0baf0dbb993edd8544f38e4dd
tanishkakakkar-max <tanishka.kakkar@sjsu.edu>
2025-10-09 23:30:29 -0700

    First commit, base class

--- CoffeeShop.java
+++ CoffeeShop.java
@@ -1,3 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+public class CoffeeShop {
+    private static final List<Object> menu = new ArrayList<>();
+    private static final List<Order> orders = new ArrayList<>();
+    private static final Scanner sc = new Scanner(System.in);
+    public static final int MAX_ORDERS = 100;
+}
