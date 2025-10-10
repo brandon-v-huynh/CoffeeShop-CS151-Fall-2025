13dca306502ff1fc7f52eeab2608604269944a6b
tanishkakakkar-max <tanishka.kakkar@sjsu.edu>
2025-10-09 23:18:16 -0700

    Starting my commit

--- Order.java
+++ Order.java
@@ -1,2 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
+
+public class Order {
+    private static int counter = 1;
+    private int orderId;
+    private Customer customer;
+    private List<Object> items;
+    private List<Integer> quantities;
+    private String status;
+    public static final int MAX_ITEMS_PER_ORDER = 10;
+}
