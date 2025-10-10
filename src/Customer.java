53e2c05fd8d55608932ffde115e15abe2c6c9d2d
2006-sk <shresthkarnani@gmail.com>
2025-10-09 22:05:13 -0700

    adding the constructor

--- src/Customer.java
+++ src/Customer.java
@@ -1,3 +1,8 @@
-public class Customer {
-    
+public class Customer extends Person {
+    private int loyaltyPoints;
+
+    public Customer(String name, String phone) {
+        super(name, phone);
+        this.loyaltyPoints = 0;
+    }
 }
